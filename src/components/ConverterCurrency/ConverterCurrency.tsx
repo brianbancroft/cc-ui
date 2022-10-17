acdecd5e3169a695d6a71395e4575f5348aa1a08
brianbancroft <hello@brianbancroft.ca>
2022-10-16 17:37:48 -0700

    Produce working piece of kit

--- src/components/ConverterCurrency/ConverterCurrency.tsx
+++ src/components/ConverterCurrency/ConverterCurrency.tsx
@@ -10,11 +10,27 @@ type CurrencyOption = {
 type CurrencySelectorProps = {
   currencyList: CurrencyOption[]
   name: string
+  handleSelect: () => void
 }
 
-const CurrencySelector = ({ currencyList, name }: CurrencySelectorProps) => {
+type NewAmount = {
+  from: string
+  to: string
+  amount: number
+  rate: number
+}
+
+const CurrencySelector = ({
+  currencyList,
+  name,
+  handleSelect,
+}: CurrencySelectorProps) => {
   return (
-    <select name={name}>
+    <select
+      className="border-l p-2 pr-8 bg-white"
+      name={name}
+      onChange={handleSelect}
+    >
       {currencyList.map((currency: CurrencyOption) => (
         <option key={currency.code} value={currency.code}>
           {currency.label}
@@ -26,10 +42,17 @@ const CurrencySelector = ({ currencyList, name }: CurrencySelectorProps) => {
 
 function ConverterCurrency() {
   const [currencyList, setCurrencyList] = useState([])
-  const [initialLoading, setInitialLoading] = useState(true)
-  const [loadingError, setLoadingError] = useState(false)
 
-  const [newAmount, setNewAmount] = useState(null)
+  const [newAmount, setNewAmount] = useState<NewAmount>({
+    from: '',
+    to: '',
+    amount: -1,
+    rate: -1,
+  })
+
+  function clearResults() {
+    setNewAmount({ ...newAmount, amount: -1 })
+  }
 
   useEffect(() => {
     const retrieveCountries = async () => {
@@ -50,33 +73,73 @@ function ConverterCurrency() {
 
     if (from === to) return
 
+    if (from === newAmount?.from && to === newAmount?.to) {
+      setNewAmount({ ...newAmount, amount: amount * newAmount.rate })
+      return
+    }
+
     const params = new URLSearchParams({ from, to })
 
     const {
       data: { rate },
     } = await api.get(`/currencies/rate?${params.toString()}`)
 
-    setNewAmount(rate)
+    setNewAmount({ rate, from, to, amount: amount * rate })
   }
 
   return (
-    <section className="w-full h-full flex justify-center">
+    <section className="w-full h-full flex justify-center p-8">
       <form className="flex flex-col" onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor="currencyFrom">from</label>
-          <CurrencySelector currencyList={currencyList} name="currencyFrom" />
+        <div className="border rounded grid grid-cols-[100px_1fr] items-center my-1">
+          <label
+            htmlFor="currencyFrom"
+            className="px-2 capitalize font-semibold"
+          >
+            from
+          </label>
+          <CurrencySelector
+            currencyList={currencyList}
+            name="currencyFrom"
+            handleSelect={clearResults}
+          />
         </div>
-        <div>
-          <label htmlFor="currencyTo">to</label>
-          <CurrencySelector currencyList={currencyList} name="currencyTo" />
+        <div className="border rounded grid grid-cols-[100px_1fr] items-center my-1">
+          <label htmlFor="currencyTo" className="px-2 capitalize font-semibold">
+            to
+          </label>
+          <CurrencySelector
+            currencyList={currencyList}
+            name="currencyTo"
+            handleSelect={clearResults}
+          />
         </div>
-        <div>
-          <label htmlFor="amount">amount</label>
-          <input type="number" name="amount" />
+        <div className="border rounded grid grid-cols-[100px_1fr] items-center my-1">
+          <label htmlFor="amount" className="px-2 capitalize font-semibold">
+            amount
+          </label>
+          <input type="number" name="amount" className="p-2 border-l" />
         </div>
-        <p>submit</p>
-        <input type="submit" name="submit" />
+        <input
+          type="submit"
+          name="submit"
+          className="p-2 bg-blue-500 rounded text-white"
+        />
       </form>
+      <div className="border rounded grid grid-cols-[200px_1fr] items-center my-1 ml-2">
+        <label htmlFor="amount" className="px-2 capitalize font-semibold">
+          converted amount
+        </label>
+        <input
+          readOnly
+          value={
+            newAmount.amount === -1
+              ? 0
+              : Math.round(newAmount.amount * 100) / 100
+          }
+          name="amount"
+          className="p-2 border-l focus:ring-0 focus:ring-offset-0 focus-visible:ring-0 focus-visible:ring-offset-0 focus-visible:outline-none"
+        />
+      </div>
     </section>
   )
 }
