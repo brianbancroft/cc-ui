9c7590256a6277fd9abca86139b1ed8da7db9199
brianbancroft <hello@brianbancroft.ca>
2022-10-16 18:14:08 -0700

    Drop boilerplate

--- src/util/api.ts
+++ src/util/api.ts
@@ -3,5 +3,4 @@ import axios from 'axios'
 export const api = axios.create({
   baseURL: `${import.meta.env['VITE_API_SERVER']}/api/v1`,
   timeout: 1000,
-  //   headers: { 'X-Custom-Header': 'foobar' },
 })
